967554f067d6e8418657582f5a80b1a5381580c4
ישראל כהנוב <israelcahanov@gmail.com>
2021-04-07 18:05:51 +0300

    commit Signed-off-by: ישראל כהנוב <israelcahanov@gmail.com>

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        //Game board 3X3 locations example
+        //0,0 0,1 0,2    
+        //1,0 1,1 1,2 
+        //2,0 2,1 2,2 
+        static void Main(string[] args)
+        {
+            XOBoard xoBoard = new XOBoard();
+            // X Wins example
+            //putAndDisplay(xoBoard, 0, 0);
+            //putAndDisplay(xoBoard, 1, 0);
+            //putAndDisplay(xoBoard, 0, 1);
+            //putAndDisplay(xoBoard, 2, 0);
+            //putAndDisplay(xoBoard, 0, 2);
+
+            // O Wins example right diagnal
+            //putAndDisplay(xoBoard, 0, 0);
+            //putAndDisplay(xoBoard, 1, 1);
+            //putAndDisplay(xoBoard, 1, 2);
+            //putAndDisplay(xoBoard, 2, 0);
+            //putAndDisplay(xoBoard, 0, 1);
+            //putAndDisplay(xoBoard, 0, 2);
+
+            //Draw
+            //putAndDisplay(xoBoard, 0, 1);
+            //putAndDisplay(xoBoard, 0, 0);
+            //putAndDisplay(xoBoard, 1, 1);
+            //putAndDisplay(xoBoard, 0, 2);
+            //putAndDisplay(xoBoard, 2, 2);
+            //putAndDisplay(xoBoard, 2, 1);
+            //putAndDisplay(xoBoard, 2, 0);
+            //putAndDisplay(xoBoard, 1, 2);
+            //putAndDisplay(xoBoard, 1, 0);
+
+            Console.ReadKey();
+        }
+
+        public static void putAndDisplay(XOBoard xoBoard, int row, int col)
+        {
+            xoBoard.Put(row, col);
+            xoBoard.Display();
+            
+            Console.WriteLine($"GameStatus: {xoBoard.Status()}" );
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ConsoleUI/XOBoard.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class XOBoard
+    {
+        public XOSymbols[,] board;
+        public const int length = 3;
+        public GameStatus gameStatus = GameStatus.None;
+        public XOBoard()
+        {
+            board = new XOSymbols[length, length];
+
+        }
+
+        private static int turn = 1;
+        public void Put(int row, int col)
+        {
+            //check if game over
+            if(gameStatus != GameStatus.None)
+            {
+                Console.WriteLine("Game Over");
+                return;
+            }
+
+            //check if location available
+            if (board[row, col] != XOSymbols.N)
+            {
+                Console.WriteLine("Can't put in this location.");
+                return; //get out of put function
+            }
+
+            //determine which turn it is and make a X or Y symbol
+            XOSymbols currentSymbol = XOSymbols.X;
+            if(turn%2 == 0)
+            {
+                currentSymbol = XOSymbols.O;
+            }
+            board[row, col] = currentSymbol;
+            turn++;
+
+            //minimum turn you can check for win
+            if(turn < length*2-1)
+            {
+                return;
+            }
+
+            int count = 0;
+            //check row
+            for (int i = 0; i < length; i++)
+            {
+                if(board[row, i] == currentSymbol)
+                {
+                    count++;
+                }
+            }
+            if(count == length)
+            {
+                gameStatus = (currentSymbol == XOSymbols.X) ? GameStatus.X : GameStatus.O;
+                return;
+            }
+            count = 0;
+
+            //check up and down
+            for (int i = 0; i < length; i++)
+            {
+                if (board[i, col] == currentSymbol)
+                {
+                    count++;
+                }
+            }
+            if (count == length)
+            {
+                gameStatus = (currentSymbol == XOSymbols.X) ? GameStatus.X : GameStatus.O;
+                return;
+            }
+            count = 0;
+
+            //check left diagonal
+            if (col == row)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (board[i, i] == currentSymbol)
+                    {
+                        count++;
+                    }
+                }
+                if (count == length)
+                {
+                    gameStatus = (currentSymbol == XOSymbols.X) ? GameStatus.X : GameStatus.O;
+                    return;
+                }
+            }
+            count = 0;
+
+            //check right diagonal
+            if (row + col == length-1)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (board[i, length - 1 - i] == currentSymbol)
+                    {
+                        count++;
+                    }
+                }
+                if (count == length)
+                {
+                    gameStatus = (currentSymbol == XOSymbols.X) ? GameStatus.X : GameStatus.O;
+                    return;
+                }
+            }
+            
+            //draw
+            if (turn == length * length+1)
+            {
+                gameStatus = GameStatus.Draw;
+                return;
+            }
+            
+        }
+
+        public GameStatus Status()
+        {
+            return gameStatus;
+        }
+
+        public void Display()
+        {
+            for (int row = 0; row < length; row++)
+            {
+                for (int col = 0; col < length; col++)
+                {
+                    Console.Write($"{board[row,col]} ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+    public enum XOSymbols 
+    {
+        //N means empty square
+        N, X, O
+    }
+    public enum GameStatus
+    {
+        X, O, None, Draw
+    }
+}
